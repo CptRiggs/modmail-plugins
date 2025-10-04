5afc460016a4f8a874f46a72c654fbb57e697095
CptRiggs <122716901+CptRiggs@users.noreply.github.com>
2025-10-04 19:25:26 +0530

    fix(steal): rename class

--- steal/steal.py
+++ steal/steal.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from random import choice as select
-class Say(commands.Cog):
+class Steal(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -51,4 +51,4 @@ class Say(commands.Cog):
         await ctx.send(message.replace("@everyone", "@.everyone").replace("@here", "@.here").replace("<@&581197130983800852>", "@.TheSonicMaster").replace("<@&581196949383020584>", "@.Staff").replace("<@&768749656388796436>", "@.Bot Developer").replace("<@&869259187719393320>", "@.Graphic Designer").replace("<@&852675563038179368>", "@.Server Booster").replace("<@&637739972174151689>", "@.VIP").replace("<@&631154626993061902>", "@.MC Map Contributor").replace("<@&819284703987236906>", "@.Super Secret").replace("<@&757188660742455296>", "@.Mew").replace("<@&837010087830618162>", "@.Chad").replace("<@&639473367316955171>", "@.Secret").replace("<@&639546711387144195>", "@.oof").replace("<@&592036135774060551>", "@.Canary").replace("<@&627463051712659494>", "@.Nostalgic").replace("<@&690902448478617631>", "@.Artist").replace("<@&618879701326233600>", "@.Gamer").replace("<@&589110560344375297>", "@.Hacker").replace("<@&639415893113307138>", "@.Musician").replace("<@&798870960308486156>", "@.Photographer").replace("<@&591521552453861386>", "@.human").replace("<@&581196486533054504>", "@.Member"))
 
 async def setup(bot):
-    await bot.add_cog(Say(bot))
+    await bot.add_cog(Steal(bot))
