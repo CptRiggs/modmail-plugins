8a0c921276ac096b8c65eaed67c6b7f47b10b205
CptRiggs <122716901+CptRiggs@users.noreply.github.com>
2025-10-04 19:48:04 +0530

    rename

--- eightball/eightball.py
+++ eightball/eightball.py
@@ -11,7 +11,7 @@ class eightball(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases = ['8ball', 'eightball'])
+    @commands.command(aliases = ['8ball'])
     async def eightball(self, ctx, *, question):
         """
         Ask me a yes/no question and get an instant answer!
