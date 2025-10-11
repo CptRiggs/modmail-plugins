5968892c552287d80de56db2408377a1f911bc52
CptRiggs <122716901+CptRiggs@users.noreply.github.com>
2025-10-11 22:29:18 +0530

    fix(say)

--- say/say.py
+++ say/say.py
@@ -9,14 +9,14 @@ class Say(commands.Cog):
     @commands.Cog.listener()
     async def on_message_delete(message):
         if message in self.message_map:
-            await self.message_map[message].delete()
+            await self.message_map[message.id].delete()
 
 
     @commands.command()
     async def say(self, ctx, *, message):
         """Say plugin that doesn't delete the original message!"""
         bot_message = await ctx.send(message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
-        self.message_map[message.id] = bot_message
+        self.message_map[ctx.message.id] = bot_message
 
 async def setup(bot):
     await bot.add_cog(Say(bot))
