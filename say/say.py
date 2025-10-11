16eee3e5de902286223e8f076fb2d6ec39ac3e50
CptRiggs <122716901+CptRiggs@users.noreply.github.com>
2025-10-11 18:05:58 +0530

    update say

--- say/say.py
+++ say/say.py
@@ -4,11 +4,19 @@ from discord.ext import commands
 class Say(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+        self.message_map = {}
+
+    @commands.Cog.listener()
+    async def on_message_delete(message):
+        if message.id in self.message_map:
+            await self.message_map[message.id].delete()
+
+
     @commands.command()
     async def say(self, ctx, *, message):
         """Say plugin that doesn't delete the original message!"""
-        await ctx.send(message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
+        bot_message = await ctx.send(message.replace("@everyone", "@\u200beveryone").replace("@here", "@\u200bhere"))
+        self.message_map[message.id] = bot_message
 
 async def setup(bot):
     await bot.add_cog(Say(bot))
